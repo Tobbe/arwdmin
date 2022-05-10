0586dad185c5c41157c69de4ff7e3205c9553ab9
Tobbe Lundberg <tobbe@tlundberg.com>
2022-05-10 10:09:48 +0200

    Initial commit

---
+++ arwdmin.ts
@@ -0,0 +1,290 @@
+// Run me with
+// npx @digitak/esrun arwdmin.ts
+
+import fs from "fs";
+import path from "path";
+import ejs from "ejs";
+import prismaSdk from "@prisma/sdk";
+import pascalcase from "pascalcase";
+
+import { pluralize } from "./lib/rwPluralize";
+
+function ejsRender(template: string, data: any) {
+  // Return type of render depends on opts.async. We're not specifying it, and
+  // it default to `false`, so we know the return type will be `string`
+  return ejs.render(template, data, {}) as string;
+}
+
+const { getDMMF } = prismaSdk;
+
+console.log("aRWdmin v0.1.0");
+
+const rwRoot = findRwRoot(path.join(process.cwd(), "..", "acm-store-rw"));
+const modelNames = await getModelNames(rwRoot);
+const pagesPath = createArwdminPagesDir(rwRoot);
+const layoutPath = createArwdminLayoutDir(rwRoot);
+await createModelPages(pagesPath, modelNames);
+createLayout(layoutPath, modelNames);
+updateRoutes(rwRoot, modelNames);
+
+/*
+ * Returns the DMMF defined by `prisma` resolving the relevant `schema.prisma` path.
+ */
+function getSchemaDefinitions(rwRoot: string) {
+  // TODO: read 'api' name from redwood.toml
+  const schemaPath = path.join(rwRoot, "api", "db", "schema.prisma");
+
+  return getDMMF({ datamodelPath: schemaPath });
+}
+
+async function getModelNames(rwRoot: string) {
+  const schema = await getSchemaDefinitions(rwRoot);
+  const modelNames: string[] = [];
+
+  for (const model of schema.datamodel.models) {
+    modelNames.push(model.name);
+  }
+
+  return modelNames;
+}
+
+function findRwRoot(dir = process.cwd()): string {
+  if (fs.existsSync(path.join(dir, "redwood.toml"))) {
+    return dir;
+  }
+
+  return findRwRoot(dir.split(path.sep).slice(0, -1).join(path.sep));
+}
+
+function createArwdminPagesDir(rwRoot: string) {
+  // TODO: read 'web' name from redwood.toml
+  const pagesPath = path.join(rwRoot, "web", "src", "pages", "Arwdmin");
+
+  // TODO: Prompt if this dir already exists if this is the first time running
+  // aRWdmin
+  // Begin by deleting to make sure we're removing pages for models that the
+  // user has removed
+  fs.rmSync(pagesPath, { recursive: true, force: true });
+  fs.mkdirSync(pagesPath);
+
+  return pagesPath;
+}
+
+// TODO: Skip (and warn) models that don't have an id column
+// TODO: Test with pokemon.
+//       Test with DVD (all-caps model).
+//       Test with QRCode (partly all-caps).
+//       Test with single-character model names, both upper- and lowercase
+function createModelPages(pagesPath: string, modelNames: string[]) {
+  for (const modelName of modelNames) {
+    console.log("Creating pages for", modelName);
+
+    const modelNameVariants = getModelNameVariants(modelName);
+
+    const modelListPage = generateModelListPage({
+      pascalCasePluralName: modelNameVariants.pascalCasePluralModelName,
+    });
+    // const modelListCell = generateModelListCell(modelNameVariants);
+    const modelListComponent = generateModelListComponent(modelNameVariants);
+    // const modelPage = generateModelComponent(modelNameVariants);
+    // const modelCell = generateModelComponent(modelNameVariants);
+    // const modelComponent = generateModelComponent(modelNameVariants);
+
+    fs.mkdirSync(
+      path.join(
+        pagesPath,
+        modelNameVariants.pascalCaseModelName,
+        modelNameVariants.pascalCasePluralModelName
+      ),
+      { recursive: true }
+    );
+
+    fs.writeFileSync(
+      path.join(
+        pagesPath,
+        modelNameVariants.pascalCaseModelName,
+        modelNameVariants.pascalCasePluralModelName,
+        modelNameVariants.pascalCasePluralModelName + "Page.tsx"
+      ),
+      modelListPage
+    );
+
+    fs.writeFileSync(
+      path.join(
+        pagesPath,
+        modelNameVariants.pascalCaseModelName,
+        modelNameVariants.pascalCasePluralModelName,
+        modelNameVariants.pascalCasePluralModelName + ".tsx"
+      ),
+      modelListComponent
+    );
+  }
+}
+
+interface ModelNameVariants {
+  modelName: string;
+  pascalCaseModelName: string;
+  pluralModelName: string;
+  pascalCasePluralModelName: string;
+}
+
+function getModelNameVariants(modelName: string): ModelNameVariants {
+  return {
+    modelName,
+    pluralModelName: modelName + "s",
+    pascalCaseModelName: pascalcase(modelName),
+    pascalCasePluralModelName: pluralize(pascalcase(modelName)),
+  };
+}
+
+function generateModelListPage({
+  pascalCasePluralName,
+}: {
+  pascalCasePluralName: string;
+}): string {
+  const template: string = fs.readFileSync("./modelListPage.ejs", "utf-8");
+
+  return ejsRender(template, { model: { pascalCasePluralName } });
+}
+
+function generateModelListComponent({
+  modelName,
+  pascalCaseModelName,
+  pluralModelName,
+  pascalCasePluralModelName,
+}: ModelNameVariants) {
+  const model = {
+    name: modelName,
+    pascalCaseName: pascalCaseModelName,
+    pluralName: pluralModelName,
+    pascalCasePluralName: pascalCasePluralModelName,
+  };
+
+  // TODO: Make sure the sr-only css class exists
+  const template = fs.readFileSync("./modelListComponent.ejs", "utf-8");
+
+  return ejsRender(template, { model });
+}
+
+function updateRoutes(rwRoot: string, modelNames: string[]) {
+  let routesPath = path.join(rwRoot, "web", "src", "Routes.tsx");
+
+  if (!fs.existsSync(routesPath)) {
+    routesPath = path.join(rwRoot, "web", "src", "Routes.js");
+  }
+
+  console.log("About to update", routesPath);
+
+  const routesFileLines = fs.readFileSync(routesPath).toString().split("\n");
+
+  const existingLayoutImportIndex = findLastIndex(routesFileLines, (line) =>
+    /^\s*import \w+Layout from/.test(line)
+  );
+
+  console.log("Current routes file", routesFileLines);
+  console.log("Last layout import", routesFileLines[existingLayoutImportIndex]);
+
+  const hasArwdminLayoutImport = !!routesFileLines.find((line) =>
+    /^\s*import ArwdminLayout from/.test(line)
+  );
+
+  if (!hasArwdminLayoutImport) {
+    routesFileLines.splice(
+      existingLayoutImportIndex + 1,
+      0,
+      "import ArwdminLayout from 'src/layouts/ArwdminLayout'"
+    );
+  }
+
+  const routerEndIndex = routesFileLines.findIndex((line) =>
+    /^\s*<\/Router>/.test(line)
+  );
+  const indent =
+    "  " + routesFileLines[routerEndIndex]?.match(/^(\s*)/)?.[1] ?? "";
+
+  console.log("routerEndIndex", routerEndIndex);
+  console.log("routerEndIndex line", routesFileLines[routerEndIndex]);
+
+  let arwdminRoutesBeginIndex = routerEndIndex - 1;
+
+  // If the "notfound" page is currently last, let's keep it there
+  if (/^\s*<Route\s.*\snotfound\s/.test(routesFileLines[routerEndIndex - 1] || '')) {
+    arwdminRoutesBeginIndex--;
+  }
+
+  routesFileLines.splice(
+    arwdminRoutesBeginIndex,
+    0,
+    `${indent}<Set wrap={ArwdminLayout}>`,
+    ...modelNames.map(
+      (name) =>
+        `${indent}  <Route path="/arwdmin/${pluralize(
+          name
+        )}" page={Arwdmin${pluralize(name)}Page} name="arwdmin${pluralize(
+          name
+        )} />"`
+    ),
+    `${indent}</Set>`
+  );
+
+  console.log("New routes file", routesFileLines);
+}
+
+/**
+ * Returns the index of the last element in the array where predicate is true, and -1
+ * otherwise.
+ * @param array The source array to search in
+ * @param predicate find calls predicate once for each element of the array, in descending
+ * order, until it finds one where predicate returns true. If such an element is found,
+ * findLastIndex immediately returns that element index. Otherwise, findLastIndex returns -1.
+ *
+ * From https://stackoverflow.com/a/53187807/88106
+ */
+function findLastIndex<T>(
+  array: Array<T>,
+  predicate: (value: T, index: number, obj: T[]) => boolean
+): number {
+  let l = array.length;
+
+  while (l--) {
+    const arrayElement = array[l]
+    if (arrayElement !== undefined && predicate(arrayElement, l, array)) {
+      return l;
+    }
+  }
+
+  return -1;
+}
+
+function generateLayout(modelNames: string[]) {
+  const template: string = fs.readFileSync("./modelListPage.ejs", "utf-8");
+
+  return ejsRender(template, { pluralModelNames: modelNames.map(pluralize) });
+}
+
+function createArwdminLayoutDir(rwRoot: string) {
+  // TODO: read 'web' name from redwood.toml
+  const layoutPath = path.join(
+    rwRoot,
+    "web",
+    "src",
+    "layouts",
+    "ArwdminLayout"
+  );
+
+  // TODO: Prompt if this dir already exists if this is the first time running
+  // aRWdmin
+  if (!fs.existsSync(layoutPath)) {
+    fs.mkdirSync(layoutPath, { recursive: true });
+  }
+
+  return layoutPath;
+}
+
+// TODO: Filter models first, then pass the same list here and to `createModelPages`
+function createLayout(layoutPath: string, modelNames: string[]) {
+  // const modelNameVariants = getModelNameVariants(modelName)
+  const layout = generateLayout(modelNames);
+
+  fs.writeFileSync(path.join(layoutPath, "ArwdminLayout.tsx"), layout);
+}
---
+++ lib/rwPluralize.ts
@@ -0,0 +1,116 @@
+import plurals from "pluralize";
+
+interface Mappings {
+  toSingular: Record<string, string>;
+  toPlural: Record<string, string>;
+}
+
+const mappings: Mappings = {
+  toSingular: {},
+  toPlural: {},
+};
+
+/**
+ * Find Bar in FooBazBar
+ */
+function lastWord(str: string) {
+  const capitals = str.match(/[A-Z]/g);
+  let lastIndex = -1
+
+  const lastCapital = capitals?.slice(-1)[0]
+  if (lastCapital) {
+    lastIndex = str.lastIndexOf(lastCapital);
+  }
+
+  return lastIndex >= 0 ? str.slice(lastIndex) : str;
+}
+
+/**
+ * Returns the plural form of the given word
+ */
+export function pluralize(word: string): string {
+  const existingMapping = mappings.toPlural[word]
+  if (existingMapping) {
+    return existingMapping;
+  }
+
+  // Sometimes `word` is a PascalCased multi-word, like FarmEquipment
+  // In those cases we only want to pass the last word on to the `pluralize`
+  // library
+  const singular = lastWord(word);
+  const base = word.slice(0, word.length - singular.length);
+
+  if (mappings.toPlural[singular]) {
+    return base + mappings.toPlural[singular];
+  }
+
+  return base + plurals.plural(singular);
+}
+
+/**
+ * Returns the singular form of the given word
+ */
+export function singularize(word: string) {
+  if (mappings.toSingular[word]) {
+    return mappings.toSingular[word];
+  }
+
+  const plural = lastWord(word);
+  const base = word.slice(0, word.length - plural.length);
+
+  if (mappings.toSingular[plural]) {
+    return base + mappings.toSingular[plural];
+  }
+
+  return base + plurals.singular(plural);
+}
+
+export function isPlural(word: string) {
+  return plurals.isPlural(lastWord(word));
+}
+
+export function isSingular(word: string) {
+  return plurals.isSingular(lastWord(word));
+}
+
+/**
+ * Adds a new mapping from singular to plural
+ *
+ * With the input 'pokemon', 'pokemonii' we'll add mappings for singular
+ * 'pokemon' to plural 'pokemonii'. We also add the reverse, so plural
+ * 'pokemonii' to singular 'pokemon'. We *also* have to make sure that
+ * the singular of 'pokemon' is 'pokemon', and that the plural of
+ * 'pokemonii' is 'pokemonii'
+ *
+ * After calling with 'pokemon', 'pokemonii' we'll have this
+ *
+ * mappings = {
+ *   toSingular: {
+ *     pokemonii: 'pokemon',
+ *     pokemon: 'pokemon'
+ *   },
+ *   toPlural: {
+ *     pokemon: 'pokemonii',
+ *     pokemonii: 'pokemonii'
+ *   }
+ * }
+ *
+ * Furthermore we need to handle changing the mappings (this is only done in
+ * tests). So if the method is called again, with input 'pokemon', 'pokemons'
+ * all the old mappings first have to be removed, before adding the new ones
+ */
+export function addSingularPlural(singular: string, plural: string) {
+  const existingPlural = Object.keys(mappings.toSingular).find(
+    (key) => mappings.toSingular[key] === singular
+  );
+
+  if (existingPlural) {
+    delete mappings.toSingular[existingPlural];
+    delete mappings.toPlural[existingPlural];
+  }
+
+  mappings.toPlural[singular] = plural;
+  mappings.toPlural[plural] = plural;
+  mappings.toSingular[plural] = singular;
+  mappings.toSingular[singular] = singular;
+}
